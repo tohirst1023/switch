c49a7bfcfc6d915f56f33657c7c0083b5a131bba
Tohir Arapjonov <toxirarapjonov@gmail.com>
2024-06-20 15:20:47 +0500

    dsdgsfb

---
+++ index.js
@@ -0,0 +1,54 @@
+let kun = prompt(" soni kiritng");
+switch (kun) {
+  case "1":
+    alert(`1  -> dushanba`);
+    break;
+  case "2":
+    alert(`2  -> seshanba`);
+    break;
+
+  case "3":
+    alert(`3  -> chorshanba`);
+    break;
+
+  case "4":
+    alert(`4  -> payshanba`);
+    break;
+
+  case "5":
+    alert(`5  -> juma`);
+    break;
+
+  case "6":
+    alert(`6  -> shanba`);
+    break;
+
+  case "7":
+    alert(` 7 -> yakshanba`);
+    default:
+        alert("Bunday hafta kunlar yoq")
+}
+
+// if (kun === "monday") {
+//     alert (`monday  -> dushanba`)
+// } else if (kun === "tuesday") {
+//     alert (`tuesday  -> seshanba`)
+
+// }
+// if (kun === "monday") {
+//     alert (`wendesday  -> Chorshanba`)
+// } else if (kun === "thursday") {
+//     alert (` thursday -> payshanba`)
+
+// }
+
+// if (kun === "friday") {
+//     alert (`friday  -> Juma`)
+// } else if (kun === "saturday") {
+//     alert (`saturday  -> shanba`)
+
+// }
+
+// if (kun === "sunday") {
+//     alert (`sunday  -> yakshanba `)
+// }
